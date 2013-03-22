67356d742629dd5b052e151e030ba5f7f2dc11af
Johan Allansson <allansson@gmail.com>
2013-03-22 21:56:39 +0100

    Implemented most of the base functionality
    	modified:   src/swan.js

--- src/swan.js
+++ src/swan.js
@@ -1,7 +1,356 @@
-/**
- * Created with JetBrains WebStorm.
- * User: allansson
- * Date: 2013-03-22
- * Time: 11:24
- * To change this template use File | Settings | File Templates.
- */
+(function (global) {
+    "use strict";
+
+    function complexTypeEvaluator(mixins, signature) {
+        function evaluateMixins(val, mixins) {
+            var i = 0;
+
+            for(i = 0; i < mixins.length; ++i) {
+                if(!swan.is(val, mixins[i])) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        function evaluateSignature(val, signature) {
+            var prop = null;
+
+            for(prop in signature) {
+                if(signature.hasOwnProperty(prop)) {
+                    if(!swan.is(val[prop], signature[prop])) {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        return function(val) {
+            return evaluateSignature(val, signature) && evaluateMixins(val, mixins);
+        };
+    }
+
+    var definitions = {},
+        swan = function(val) {
+
+            return {
+                expect: function(archetypeN) {
+                    swan.expect(val, archetypeN);
+                },
+                is: function(archetypeN) {
+                    return swan.is(val, archetypeN);
+                },
+                as: function(archetypeN) {
+                    return swan.as(val, archetypeN);
+                }
+            };
+        };
+
+    swan.define = function(archetype, definition) {
+        var i = 0,
+            mixins = null,
+            signature = null,
+            evaluator = null,
+            
+            prop = null;
+
+        if(!swan.is(definitions[archetype], 'undefined')) {
+            throw new Error("An archetype with name '" + archetype + "' already exists.");
+        }
+
+        if(!swan.is(archetype, 'string')) {
+            throw new Error('Argument archetype must be of type string.');
+        }
+
+        if(swan.is(definition, 'array')) {
+            definition = {
+                mixins: definition
+            };
+        }
+
+        if(swan.is(definition, 'function')) {
+            definition = {
+                evaluator: definition
+            }
+        }
+
+        if(swan.is(definition, 'object') && swan.is(definition.mixins, 'undefined') && swan.is(definition.signature, 'undefined') && swan.is(definition.evaluator, 'undefined')) {
+            definition = {
+                signature: definition
+            };
+        }
+
+        if(!swan.is(definition, 'object')) {
+            throw new Error("Argument 'definition' must be a definition object, a plain object, an array of mixins or an evaluator function.");
+        }
+
+        mixins = definition.mixins || [];
+        signature = definition.signature || {};
+        evaluator = definition.evaluator;
+
+        if(swan.is(mixins, 'string')) {
+            mixins = [mixins];
+        } else if (!swan.is(mixins, 'array')) {
+            throw new Error("Property 'mixins' must be an array or a string.");
+        }
+        
+        if(!swan.is(signature, 'object')) {
+            throw new Error("Property 'signature' must be a map.")
+        }
+
+        if(!evaluator) {
+            for(prop in signature) {
+                if(signature.hasOwnProperty(prop)) {
+                    if(swan.is(signature[prop], 'array')) {
+                        for(i = 0; i < signature[prop].length; ++i) {
+                            if(!swan.is(signature[prop[i]], 'string')) {
+                                throw new Error("Array in property '" + prop + "' of 'signature' may only contain strings.");
+                            }
+                        }
+                    } else if(!swan.is(signature[prop], 'string')) {
+                        throw new Error("Property '" + prop + "' of 'signature' must be a string or an array of strings.")
+                    }
+                }
+            }
+
+            evaluator = complexTypeEvaluator(mixins, signature);
+        }
+
+        if(!swan.is(evaluator, 'function')) {
+            throw new Error("Property 'evaluator' must be a function.");
+        }
+
+        definitions[archetype] = {
+            mixins: mixins,
+            signature: signature,
+            evaluator: evaluator
+        };
+    }
+
+    swan.globalize = function(toggle) {
+        if(swan.is(toggle, 'undefined')) {
+            toggle = true;
+        }
+
+        if(toggle) {
+            if(swan.is(Object.prototype.is, 'undefined')) {
+                Object.defineProperty(Object.prototype, "is", {
+                    value: function(archetypeN) {
+                        return swan.is(this, archetypeN);
+                    },
+                    configurable: true,
+                    writable: false,
+                    enumerable: false
+                });
+            }
+
+            if(swan.is(Object.prototype.as, 'undefined')) {
+                Object.defineProperty(Object.prototype, "as", {
+                    value: function(archetypeN) {
+                        return swan.as(this, archetypeN);
+                    },
+                    configurable: true,
+                    writable: false,
+                    enumerable: false
+                });
+            }
+
+            if(swan.is(Object.prototype.expect, 'undefined')) {
+                Object.defineProperty(Object.prototype, "expect", {
+                    value: function(archetype, errMsg) {
+                        return swan.expect(this, archetype, errMsg);
+                    },
+                    configurable: true,
+                    writable: false,
+                    enumerable: false
+                });
+            }
+        } else {
+            if(swan.is(Object.prototype.is, 'function')) {
+                delete Object.prototype.is;
+            }
+
+            if(swan.is(Object.prototype.as, 'function')) {
+                delete Object.prototype.as;
+            }
+
+            if(swan.is(Object.prototype.expect, 'function')) {
+                delete Object.prototype.expect;
+            }
+        }
+    };
+
+    swan.use
+
+    swan.expect = function(val, archetype, errMsg) {
+        if(!swan.is(val, archetype)) {
+            throw new Error(errMsg || "Expected value to be of archetype '" +  archetype + "'.");
+        }
+    };
+
+    swan.is = function(val, archetypeN) {
+        var i = 0,
+            definition = null;
+
+        for(i = 1; i < arguments.length; i += 1) {
+            definition = definitions[arguments[i]];
+
+            if(!definition) {
+                throw new Error("Archetype '" + arguments[i] + "' does not exist. An archetype has to be defined before it can be used.");
+            }
+
+            if(!definition.evaluator(val)) {
+                return false;
+            }
+        }
+
+        return true;
+    };
+
+    swan.as = function(val, archetypeN) {
+        var i = 1,
+            proxy = {};
+
+        function functionProxy(self, f) {
+            return function() {
+                return f.call(self, arguments);
+            };
+        }
+
+        function writeProperties(val, signature, target) {
+            var prop = null;
+
+            for(prop in signature) {
+                if(signature.hasOwnProperty(prop)) {
+                    if(target.hasOwnProperty(prop)) {
+                        continue;
+                    }
+
+                    if(!swan.is(val[prop], signature[prop])) {
+                        return null;
+                    }
+
+                    if(signature[prop] === 'function') {
+                        target[prop] = functionProxy(val, val[prop]);
+                    } else {
+                        target[prop] = val[prop];
+                    }
+                }
+            }
+
+            return target;
+        }
+
+        function mergeMixins(val, mixins, target) {
+            var i = 0;
+
+            for(i = 0; i < mixins.length; i += 1) {
+                try {
+                    if(!buildProxy(val, mixins[i], target)) {
+                        return null;
+                    }
+                } catch(err) {
+                    throw new Error("Failed to infer extending archetype '" + mixins[i] + "'. Reason was: " + err.message);
+                }
+            }
+        }
+
+        function buildProxy(val, archetype, target) {
+            var definition = definitions[archetype];
+
+            target = target || {};
+
+            if(!definition) {
+                throw new Error("Archetype '" + definition + "' does not exist. An archetype has to be defined before it can be used.")
+            }
+
+            if(definition.signature && !writeProperties(val, definition.signature, target)) {
+                return null;
+            }
+
+            mergeMixins(val, definition.mixins, target);
+
+            return target;
+        }
+
+        for(i = 1; i < arguments.length; i += 1) {
+            if(!buildProxy(val, arguments[i], proxy)) {
+                return null;
+            }
+        }
+
+        return proxy;
+    };
+
+    swan.reset = function() {
+        definitions = {
+            "any": {
+                mixins: [],
+                signature: {},
+                evaluator: function(val) {
+                    return true;
+                }
+            },
+            "object": {
+                mixins: [],
+                signature: {},
+                evaluator: function(val) {
+                    return typeof(val) === 'object';
+                }
+            },
+            "string": {
+                mixins: [],
+                signature: {},
+                evaluator: function(val) {
+                    return typeof(val) === 'string';
+                }
+            },
+            "number": {
+                mixins: [],
+                signature: {},
+                evaluator: function(val) {
+                    return typeof(val) === 'number';
+                }
+            },
+            "boolean": {
+                mixins: [],
+                signature: {},
+                evaluator: function(val) {
+                    return typeof(val) === 'boolean';
+                }
+            },
+            "undefined": {
+                mixins: [],
+                signature: {},
+                evaluator: function(val) {
+                    return typeof(val) === 'undefined';
+                }
+            },
+            "array": {
+                mixins: [],
+                signature: {},
+                evaluator: (function() {
+                    var toStr = Object.prototype.toString,
+                        arrayStr = toStr.call([]);
+
+                    return function(val) {
+                        return toStr.call(val) === arrayStr;
+                    };
+                }())
+            },
+            "function": {
+                mixins: [],
+                signature: {},
+                evaluator: function(val) {
+                    return typeof(val) === 'function';
+                }
+            }
+        };
+    };
+
+    swan.reset();
+    global.swan = swan;
+}(this));
--- tests/swan-tests.js
+++ tests/swan-tests.js
@@ -1,7 +1,249 @@
-/**
- * Created with JetBrains WebStorm.
- * User: allansson
- * Date: 2013-03-22
- * Time: 11:24
- * To change this template use File | Settings | File Templates.
- */
+describe('swan.js', function() {
+    beforeEach(function() {
+        swan.reset();
+        swan.globalize(false);
+    });
+
+    describe('globalize', function() {
+        it('should define a global function is() on all objects', function() {
+            var obj = {};
+
+            swan.globalize();
+
+            obj.should.have.property('is');
+            obj.is.should.be.a('function');
+        });
+
+        it('should define a global function as() on all objects', function() {
+            var obj = {};
+
+            swan.globalize();
+
+            obj.should.have.property('is');
+            obj.as.should.be.a('function');
+        });
+
+        it('should define a global function expect() on all objects', function() {
+            var obj = {};
+
+            swan.globalize();
+
+            obj.should.have.property('is');
+            obj.expect.should.be.a('function');
+        });
+
+        it('should remove global function is() when specifying false as parameter', function() {
+             var obj = {};
+
+            swan.globalize();
+            swan.globalize(false);
+
+            obj.should.not.have.property('is');
+        });
+
+        it('should remove global function as() when specifying false as parameter', function() {
+            var obj = {};
+
+            swan.globalize();
+            swan.globalize(false);
+
+            obj.should.not.have.property('is');
+        });
+
+        it('should remove global function expect() when specifying false as parameter', function() {
+            var obj = {};
+
+            swan.globalize();
+            swan.globalize(false);
+
+            obj.should.not.have.property('is');
+        });
+    });
+
+    describe('primitive objects', function() {
+        it('should match primitive archetype string', function() {
+            var result = swan.is('my string', 'string');
+
+            result.should.equal(true);
+        })
+
+        it('should match primitive archetype number', function () {
+            var result = swan.is(10.0, 'number');
+
+            result.should.equal(true);
+        });
+
+        it('should match primitive archetype object', function () {
+            var result = swan.is({}, 'object');
+
+            result.should.equal(true);
+        });
+
+        it('should match primitive archetype boolean', function () {
+            var result = swan.is(true, 'boolean');
+
+            result.should.equal(true);
+        });
+
+        it('should match primitive archetype array', function () {
+            var result = swan.is(['string', 10], 'array');
+
+            result.should.equal(true);
+        });
+
+        it('should match primitive archetype function', function () {
+            var result = swan.is(function() {}, 'function');
+
+            result.should.equal(true);
+        });
+
+        it('should match primitive archetype undefined', function () {
+            var result = swan.is({}.undefinedProperty, 'undefined');
+
+            result.should.equal(true);
+        });
+    });
+
+    describe('complex type definition', function() {
+        it('should register archetype when giving an evaluator function', function() {
+            var result = false,
+                obj = {
+                prop: true
+            };
+
+            swan.define('evaluator_function_type', {
+               evaluator: function(val) {
+                   return obj.prop;
+               }
+            });
+
+            result = swan.is(obj, 'evaluator_function_type');
+
+            true.should.equal(result);
+        });
+
+        it('should register archetype when giving a signature', function() {
+            var result = false,
+                obj = {
+                bool: true,
+                str: 'string',
+                func: function() {
+
+                },
+                array: []
+            };
+
+            swan.define('signature_type', {
+                signature: {
+                    bool: 'boolean',
+                    str: 'string',
+                    func: 'function',
+                    array: 'array'
+                }
+            });
+
+            result = swan.is(obj, 'signature_type');
+
+            true.should.equal(result);
+        });
+
+        it('should register archetype with mixins', function() {
+            var result = false,
+
+                obj = {
+                    bool: true,
+                    str: 'string',
+                    func: function() {
+
+                    },
+                    array: []
+                };
+
+            swan.define('mixin1', {
+                str: 'string',
+                array: 'array'
+            });
+
+            swan.define('mixin2', {
+                bool: 'boolean',
+                func: 'function'
+            })
+
+            swan.define('signature_type', {
+                mixins: ['mixin1', 'mixin2']
+            });
+
+            result = swan.is(obj, 'signature_type');
+
+            true.should.equal(result);
+        });
+
+        it('should register a pure mixin archetype when passing array as definition', function() {
+            var result = false,
+                obj = {
+                    prop: true,
+                    prop2: 'str',
+                    ignored: function() {}
+                };
+
+            swan.define('mixin1', {
+                signature: {
+                    prop: 'boolean'
+                }
+            });
+
+            swan.define('mixin2', {
+                signature: {
+                    prop2: 'string'
+                }
+            });
+
+            swan.define('pure-mixin', ['mixin1', 'mixin2']);
+
+            result = swan.is(obj, 'pure-mixin');
+
+            true.should.equal(result);
+        });
+
+        it('should register a signature based archetype when passing a normal object as definition', function() {
+            var result = false,
+                obj = {
+                    prop: true,
+                    prop2: 'str',
+                    ignored: function() {}
+                };
+
+            swan.define('obj', {
+                prop: 'boolean',
+                prop2: 'string'
+            });
+
+            result = swan.is(obj, 'obj');
+
+            true.should.equal(result);
+        });
+
+        it('should register a evaluator archetype when passing function as definition', function() {
+            var result = false,
+                obj = {
+                    prop: true,
+                    prop2: 'str',
+                    ignored: function() {}
+                };
+
+            swan.define('evaluator', function(val) {
+                return swan.is(val['prop'], 'boolean');
+            });
+
+            result = swan.is(obj, 'evaluator');
+
+            true.should.equal(result);
+        });
+    });
+
+    describe('archetype casting', function() {
+        it('should return an object exposing only the archetype members', function() {
+
+        });
+    });
+})
